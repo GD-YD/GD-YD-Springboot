7e6ba56bdde9dd4547b6ade6d125e4a39c38fa32
Woojun-Yoon <dbsdnwns11@naver.com>
2024-03-22 03:04:07 +0900

    chore: Set Project multimodule

---
+++ gd-yd-api/src/main/java/com/gdyd/gdydapi/GdYdApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.gdyd.gdydapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdYdApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdYdApiSpringbootApplication.class, args);
+	}
+
+}
