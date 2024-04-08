40cf85c95499b1df1c3d3fbc42df9c21702b70a5
Woojun-Yoon <dbsdnwns11@naver.com>
2024-04-08 13:15:35 +0900

    feat: Add Config files for JPA, Swagger

---
+++ gd-yd-core/src/main/java/com/gdyd/gdydcore/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.gdyd.gdydcore.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
