49daaa2c3e410021f6731bbf7d7ab5a5b3061f23
Woojun-Yoon <dbsdnwns11@naver.com>
2024-04-08 13:14:44 +0900

    feat: Add Repositories

--- gd-yd-core/src/main/java/com/gdyd/gdydcore/repository/member/MemberRepository.java
+++ gd-yd-core/src/main/java/com/gdyd/gdydcore/repository/member/MemberRepository.java
@@ -1,4 +1,4 @@
-package com.gdyd.gdydcore.repository;
+package com.gdyd.gdydcore.repository.member;
 
 import com.gdyd.gdydcore.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
