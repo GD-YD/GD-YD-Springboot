4fe795b72b256718057842b3b91bef3b1ebcd890
Woojun-Yoon <dbsdnwns11@naver.com>
2024-04-08 13:12:08 +0900

    feat: Add Application config for multimodule

--- gd-yd-api/src/main/java/com/gdyd/gdydapi/GdYdApiApplication.java
+++ gd-yd-api/src/main/java/com/gdyd/gdydapi/GdYdApiApplication.java
@@ -2,12 +2,16 @@ package com.gdyd.gdydapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan("com.gdyd.gdydcore")
+@EnableJpaRepositories("com.gdyd.gdydcore")
+@SpringBootApplication(scanBasePackages={"com.gdyd"})
 public class GdYdApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GdYdApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GdYdApiApplication.class, args);
+    }
 
 }
