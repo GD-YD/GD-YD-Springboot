2ae7ecacc48bbf330efddaeaafdc61aa7b77ed0c
Woojun-Yoon <dbsdnwns11@naver.com>
2024-04-22 12:48:51 +0900

    feat: Create S3Config and WebSecurityConfig

---
+++ gd-yd-auth/src/main/java/com/gdyd/gdydauth/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.gdyd.gdydauth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+}
---
+++ gd-yd-infra/src/main/java/com/gdyd/gdydinfra/aws/config/S3Config.java
@@ -0,0 +1,8 @@
+package com.gdyd.gdydinfra.aws.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+}
