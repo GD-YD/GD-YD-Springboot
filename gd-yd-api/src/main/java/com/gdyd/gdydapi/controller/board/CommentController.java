7f27f729e51462922485af8afcbb9381e8b8b146
csb0710 <sdssond@gmail.com>
2024-05-19 04:15:10 +0900

    refactor: Uodate Post API for Post

--- gd-yd-api/src/main/java/com/gdyd/gdydapi/controller/board/CommentController.java
+++ gd-yd-api/src/main/java/com/gdyd/gdydapi/controller/board/CommentController.java
@@ -1,19 +1,13 @@
 package com.gdyd.gdydapi.controller.board;
 
 import com.gdyd.gdydapi.request.board.SaveCommentRequest;
-import com.gdyd.gdydapi.request.board.SavePostReqeust;
 import com.gdyd.gdydapi.response.board.SaveCommentResponse;
-import com.gdyd.gdydapi.response.board.SavePostResponse;
 import com.gdyd.gdydapi.service.board.CommentCommandService;
-import com.gdyd.gdydapi.service.board.PostCommandService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Comment", description = "Comment 관련 API")
 @RestController
@@ -25,7 +19,7 @@ public class CommentController {
     @Operation(summary = "Comment 생성 API", description = "Comment를 생성하는 API")
     @PostMapping
     public ResponseEntity<SaveCommentResponse> createComment(@RequestBody SaveCommentRequest request) {
-        SaveCommentResponse response = commentCommandService.savePost(request);
+        SaveCommentResponse response = commentCommandService.saveComment(request);
         return ResponseEntity.ok(response);
     }
 }
