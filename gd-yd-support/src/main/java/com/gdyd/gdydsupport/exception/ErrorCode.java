14be95d398cf25afecf370b9d73d3c0b31021bda
Woojun-Yoon <dbsdnwns11@naver.com>
2024-05-20 09:01:01 +0900

    feat: JwtFilter에서 얻은 access token으로 멤버 Id를 얻는 메서드 추

--- gd-yd-support/src/main/java/com/gdyd/gdydsupport/exception/ErrorCode.java
+++ gd-yd-support/src/main/java/com/gdyd/gdydsupport/exception/ErrorCode.java
@@ -30,7 +30,10 @@ public enum ErrorCode {
     AUTHENTICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "AUTHENTICATION_FORBIDDEN", "유효하지 않은 사용자 정보입니다."),
 
     INVALID_MEMBER_EMAIL(HttpStatus.BAD_REQUEST, "INVALID_MEMBER_EMAIL", "이미 가입되어 있는 회원입니다."),
-    INVALID_MEMBER_PASSWORD(HttpStatus.BAD_REQUEST, "INVALID_MEMBER_PASSWORD", "비밀번호가 일치하지 않습니다.");
+    INVALID_MEMBER_PASSWORD(HttpStatus.BAD_REQUEST, "INVALID_MEMBER_PASSWORD", "비밀번호가 일치하지 않습니다."),
+
+    INVALID_MEMBER_PROFILE_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_MEMBER_PROFILE_REQUEST", "로그인이 되어있지 않고 프로필을 조회할 수 없습니다."),
+    INVALID_PRINCIPAL(HttpStatus.BAD_REQUEST, "INVALID_PRINCIPAL", "로그인 객체의 정보가 유효하지 않거나, 존재하지 않습니다.");
 
     // AWS
 
