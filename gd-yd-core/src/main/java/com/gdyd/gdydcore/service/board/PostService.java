ee8e30bc0d7fbb846e7948068de520706c22f1c0
csb0710 <sdssond@gmail.com>
2024-05-27 01:23:59 +0900

    refactor: Refactoring code for Post

--- gd-yd-core/src/main/java/com/gdyd/gdydcore/service/board/PostService.java
+++ gd-yd-core/src/main/java/com/gdyd/gdydcore/service/board/PostService.java
@@ -1,16 +1,12 @@
 package com.gdyd.gdydcore.service.board;
 
 import com.gdyd.gdydcore.domain.board.Post;
-import com.gdyd.gdydcore.domain.member.Member;
 import com.gdyd.gdydcore.repository.board.PostRepository;
-import com.gdyd.gdydcore.repository.member.MemberRepository;
 import com.gdyd.gdydsupport.exception.BusinessException;
 import com.gdyd.gdydsupport.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
