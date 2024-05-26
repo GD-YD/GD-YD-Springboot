6e0e2d6c983347d75add29be7dbe1d0c2414cfb0
Woojun-Yoon <dbsdnwns11@naver.com>
2024-05-26 19:41:05 +0900

    feat: S3 Presigend-url 관련 로직 추가

--- gd-yd-infra/src/main/java/com/gdyd/gdydinfra/aws/config/S3Config.java
+++ gd-yd-infra/src/main/java/com/gdyd/gdydinfra/aws/config/S3Config.java
@@ -1,8 +1,34 @@
 package com.gdyd.gdydinfra.aws.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    private AWSStaticCredentialsProvider createAwsCredentialsProvider() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return new AWSStaticCredentialsProvider(basicAWSCredentials);
+    }
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        return (AmazonS3Client) AmazonS3Client.builder()
+                .withRegion(region)
+                .withCredentials(createAwsCredentialsProvider())
+                .build();
+    }
 }
