c715e930256eddb56fdf667b0105ebdd752903dd
Woojun-Yoon <dbsdnwns11@naver.com>
2024-06-03 13:41:24 +0900

    refactor: import 문 최적화

--- gd-yd-core/src/main/java/com/gdyd/gdydcore/service/board/CommentService.java
+++ gd-yd-core/src/main/java/com/gdyd/gdydcore/service/board/CommentService.java
@@ -10,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
