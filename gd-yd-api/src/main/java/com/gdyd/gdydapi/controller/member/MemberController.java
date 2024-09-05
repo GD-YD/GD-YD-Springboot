1ea0e1d7aa5acf71437ae75e07796902e0b184c3
Woojun-Yoon <dbsdnwns11@naver.com>
2024-09-06 01:44:46 +0900

    refactor: import 문 최적화

--- gd-yd-api/src/main/java/com/gdyd/gdydapi/controller/member/MemberController.java
+++ gd-yd-api/src/main/java/com/gdyd/gdydapi/controller/member/MemberController.java
@@ -7,7 +7,6 @@ import com.gdyd.gdydapi.service.member.MemberQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.sql.Update;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
