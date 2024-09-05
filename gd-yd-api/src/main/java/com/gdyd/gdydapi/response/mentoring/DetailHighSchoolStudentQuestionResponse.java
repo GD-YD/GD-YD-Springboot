3a4e412f7fbb385468ee6d5d5d42ad4b8430655e
Woojun-Yoon <dbsdnwns11@naver.com>
2024-09-06 02:45:30 +0900

    feat: 질문글 상세 조회 API를 위한 응답 구

---
+++ gd-yd-api/src/main/java/com/gdyd/gdydapi/response/mentoring/DetailHighSchoolStudentQuestionResponse.java
@@ -0,0 +1,44 @@
+package com.gdyd.gdydapi.response.mentoring;
+
+import com.gdyd.gdydcore.domain.mentoring.HighSchoolStudentQuestion;
+import com.gdyd.gdydcore.domain.mentoring.UniversityStudentAnswer;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+@Schema(description = "고등학생 질문 상세 응답")
+public record DetailHighSchoolStudentQuestionResponse(
+        @Schema(description = "고등학생 질문 ID", example = "1")
+        Long id,
+
+        @Schema(description = "고등학생 닉네임", example = "궁금한 고등학생2")
+        String highSchoolStudentNickname,
+
+        @Schema(description = "고등학생 질문 제목", example = "이 성적으로 어떤 대학을 갈수 있을까요?")
+        String title,
+
+        @Schema(description = "고등학생 질문 내용", example = "이번에 모의고사 성적이 나왔는데, 이 성적으로 어떤 대학을 갈수 있을까요?")
+        String question,
+
+        @Schema(description = "대학생 답변 목록")
+        List<UniversityStudentAnswerResponse> answers,
+
+        @Schema(description = "고등학생 질문 등록일", example = "2021-10-01T00:00:00")
+        String createdAt
+) {
+    public static DetailHighSchoolStudentQuestionResponse from(
+            HighSchoolStudentQuestion highSchoolStudentQuestion,
+            List<UniversityStudentAnswer> universityStudentAnswers
+    ) {
+        return DetailHighSchoolStudentQuestionResponse.builder()
+                .id(highSchoolStudentQuestion.getId())
+                .highSchoolStudentNickname(highSchoolStudentQuestion.getHighSchoolStudent().getNickname())
+                .title(highSchoolStudentQuestion.getTitle())
+                .question(highSchoolStudentQuestion.getQuestion())
+                .answers(universityStudentAnswers.stream().map(UniversityStudentAnswerResponse::from).toList())
+                .createdAt(highSchoolStudentQuestion.getCreatedAt().toString())
+                .build();
+    }
+}
---
+++ gd-yd-api/src/main/java/com/gdyd/gdydapi/response/mentoring/UniversityStudentAnswerResponse.java
@@ -0,0 +1,30 @@
+package com.gdyd.gdydapi.response.mentoring;
+
+import com.gdyd.gdydcore.domain.mentoring.UniversityStudentAnswer;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+@Schema(description = "대학생 답변 응답")
+public record UniversityStudentAnswerResponse(
+        @Schema(description = "대학생 답변 ID", example = "1")
+        Long id,
+
+        @Schema(description = "대학생 닉네임", example = "똑똑한 대학생3")
+        String universityStudentNickname,
+
+        @Schema(description = "대학생 답변 내용", example = "이 성적으로는 단국대가 적합할 것 같습니다.")
+        String answer,
+
+        @Schema(description = "대학생 답변 등록일", example = "2021-10-01T00:00:00")
+        String createdAt
+) {
+    public static UniversityStudentAnswerResponse from(UniversityStudentAnswer universityStudentAnswer) {
+        return UniversityStudentAnswerResponse.builder()
+                .id(universityStudentAnswer.getId())
+                .universityStudentNickname(universityStudentAnswer.getUniversityStudent().getNickname())
+                .answer(universityStudentAnswer.getAnswer())
+                .createdAt(universityStudentAnswer.getCreatedAt().toString())
+                .build();
+    }
+}
