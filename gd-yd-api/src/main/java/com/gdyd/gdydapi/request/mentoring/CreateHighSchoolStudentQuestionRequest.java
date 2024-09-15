c8b0a78e4fae00e0213d4f234b83b032bb4baa21
Woojun-Yoon <dbsdnwns11@naver.com>
2024-09-16 01:17:43 +0900

    feat: 고등학생 질문 미디어 관련 비즈니스 로직 추가

--- gd-yd-api/src/main/java/com/gdyd/gdydapi/request/mentoring/CreateHighSchoolStudentQuestionRequest.java
+++ gd-yd-api/src/main/java/com/gdyd/gdydapi/request/mentoring/CreateHighSchoolStudentQuestionRequest.java
@@ -2,8 +2,11 @@ package com.gdyd.gdydapi.request.mentoring;
 
 import com.gdyd.gdydcore.domain.member.HighSchoolStudent;
 import com.gdyd.gdydcore.domain.mentoring.HighSchoolStudentQuestion;
+import com.gdyd.gdydcore.domain.mentoring.HighSchoolStudentQuestionMedia;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
 
 @Schema(description = "고등학생 질문 등록 요청")
 public record CreateHighSchoolStudentQuestionRequest(
@@ -13,7 +16,10 @@ public record CreateHighSchoolStudentQuestionRequest(
 
         @NotBlank(message = "질문글의 내용은 필수입니다.")
         @Schema(description = "질문글 내용", example = "12344로 해당 대학에 입학할 수 있을까요?")
-        String question
+        String question,
+
+        @Schema(description = "고등학생 질문 미디어 URL")
+        List<String> highSchoolStudentQuestionMediaUrls
 ) {
         public static HighSchoolStudentQuestion toHighSchoolStudentQuestion(
                 CreateHighSchoolStudentQuestionRequest request,
@@ -24,5 +30,17 @@ public record CreateHighSchoolStudentQuestionRequest(
                         .question(request.question())
                         .highSchoolStudent(highSchoolStudent)
                         .build();
+        }
+
+        public static List<HighSchoolStudentQuestionMedia> toHighSchoolStudentQuestionMedia(
+                CreateHighSchoolStudentQuestionRequest request,
+                HighSchoolStudentQuestion highSchoolStudentQuestion
+        ) {
+                return request.highSchoolStudentQuestionMediaUrls().stream()
+                        .map(url -> HighSchoolStudentQuestionMedia.builder()
+                                .url(url)
+                                .highSchoolStudentQuestion(highSchoolStudentQuestion)
+                                .build())
+                        .toList();
         }
 }
