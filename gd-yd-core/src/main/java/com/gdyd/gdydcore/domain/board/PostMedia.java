2d0892d3f33cf5d8e89f43ef6e96cdd0bb291848
Woojun-Yoon <dbsdnwns11@naver.com>
2024-09-16 00:32:03 +0900

    refactor: PostMedia 에 BaseTimeEntity 추가

--- gd-yd-core/src/main/java/com/gdyd/gdydcore/domain/board/PostMedia.java
+++ gd-yd-core/src/main/java/com/gdyd/gdydcore/domain/board/PostMedia.java
@@ -1,5 +1,6 @@
 package com.gdyd.gdydcore.domain.board;
 
+import com.gdyd.gdydcore.domain.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -10,7 +11,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PostMedia {
+public class PostMedia extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
