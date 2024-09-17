d48663be7e53407f379383154afd2a61df6c9587
csb0710 <sdssond@gmail.com>
2024-09-17 20:14:14 +0900

    refactor: @Schema 누락 부분 추가

--- gd-yd-support/src/main/java/com/gdyd/gdydsupport/webhook/ReportDiscordMessage.java
+++ gd-yd-support/src/main/java/com/gdyd/gdydsupport/webhook/ReportDiscordMessage.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Builder
 @Schema(description = "신고 알림용 디스코드 메시지")
 public record ReportDiscordMessage(
+        @Schema(description = "디스코드 메시지 내용")
         String content
 ) {
     public static ReportDiscordMessage from(String content) {
